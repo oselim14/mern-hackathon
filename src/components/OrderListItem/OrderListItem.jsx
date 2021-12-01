b68648f16eec5e324cbc90bc1c9751ce2f5b906d
Tavi Selim <oselim14@gmail.com>
2021-11-30 16:00:43 -0800

    orderlist

--- src/components/OrderListItem/OrderListItem.jsx
+++ src/components/OrderListItem/OrderListItem.jsx
@@ -2,10 +2,19 @@ import './OrderListItem.css';
 
 export default function OrderListItem({orderItem, history}) {
   return (
+    <div className="OrderList">
     <div className="OrderListItem">
-      
-        <div>{orderItem.updatedAt}</div>
-
+      <div>
+        <div>
+          Order Id: <span className="smaller">{orderItem.orderId}</span>
+          <div className="smaller">{new Date(orderItem.updatedAt).toLocaleDateString()}</div>
+        </div>
+        </div>
+        <div className="align-rt">
+          <div>{orderItem.totalQty}</div>
+          <div className="smaller">${(orderItem.orderTotal).toFixed(2)}</div>
+        </div>
+      </div>
     </div>
   );
 }
